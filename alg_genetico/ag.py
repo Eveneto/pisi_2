3f9871901a8086dcbdf8930cc8a713860f86e3a3
Dmoraisz <lucas.morais1318@gmail.com>
2024-09-14 16:06:46 -0300

    ajustes finais

--- alg_genetico/ag.py
+++ alg_genetico/ag.py
@@ -1,20 +1,20 @@
 from random import random, randint, shuffle, sample
 import tsplib95 as tsplib
 
-def carregar_cidades(file_path):
-    tsp = tsplib.load(file_path)
+def entrada(arquivo_entrada):
+    tsp = tsplib.load(arquivo_entrada)
     return {i: coord for i, coord in enumerate(tsp.node_coords.values())}
 
-def gerar_populacao_inicial(cidades, n_pop=20):
+def gerar_populacao_inicial(cidades, n_pop):
     return [shuffle(list(cidades.keys())) or list(cidades.keys()) for _ in range(n_pop)]
 
-def calcular_distancia_total(caminho, cidades):
+def calcular_distancia(caminho, cidades):
     return sum(((cidades[caminho[i]][0] - cidades[caminho[i+1]][0])**2 + 
                 (cidades[caminho[i]][1] - cidades[caminho[i+1]][1])**2)**0.5
                for i in range(len(caminho) - 1))
 
 def avaliar_populacao(populacao, cidades):
-    return [calcular_distancia_total(ind, cidades) for ind in populacao]
+    return [calcular_distancia(ind, cidades) for ind in populacao]
 
 def selecao_por_torneio(populacao, aptidoes, k):
     selecionados = []
@@ -59,8 +59,8 @@ def mutar_individuo(individuo, taxa_mutacao, cidades):
         individuo[c1], individuo[c2] = individuo[c2], individuo[c1]
     return individuo
 
-def evoluir(cidades, taxa_cruzamento, taxa_mutacao, tamanho_torneio, n_geracoes):
-    populacao = gerar_populacao_inicial(cidades)
+def evoluir(cidades, taxa_cruzamento, taxa_mutacao, tamanho_torneio, n_geracoes , populacao):
+    populacao = gerar_populacao_inicial(cidades , populacao)
     aptidoes = avaliar_populacao(populacao, cidades)
 
     for _ in range(n_geracoes):
@@ -80,13 +80,13 @@ def selecao_elitista(populacao_atual, nova_populacao, aptidoes_atual, aptidoes_n
     return [ind for ind, _ in sobreviventes], [apt for _, apt in sobreviventes]
 
 def main():
-    cidades = carregar_cidades("pisi_2\wi29.tsp")
+    cidades = entrada("wi29.tsp")
     taxa_cruzamento = 0.8
     taxa_mutacao = 0.01
     tamanho_torneio = 4
     n_geracoes = 10000
-    
-    melhor_rota, menor_custo = evoluir(cidades, taxa_cruzamento, taxa_mutacao, tamanho_torneio, n_geracoes)
+    n_populacao = 50
+    melhor_rota, menor_custo = evoluir(cidades, taxa_cruzamento, taxa_mutacao, tamanho_torneio, n_geracoes , n_populacao)
     print(f"A melhor rota encontrada foi: {melhor_rota} com custo {menor_custo} dronômetros.")
 for i in range(30):
     main()
